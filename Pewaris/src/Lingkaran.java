3f8ad57525134fc54685073978149c27f5b2b5cb
DafinaAgustina <56856787+DafinaAgustina@users.noreply.github.com>
2019-11-20 13:56:02 +0700

    Add files via upload

---
+++ Pewaris/src/Lingkaran.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author acer
+ */
+public class Lingkaran extends Bangundatar {
+    public void Lingkaran(){
+        double ll,rl,kl,phi = 3.14;
+        Scanner b = new Scanner (System.in);
+        System.out.println("LINGKARAN");
+        System.out.println ("==========");
+        System.out.println("Masukkan jari-jari :" );
+        rl = b.nextDouble();
+        ll = phi*rl*rl;
+        kl = 2*phi*rl;
+        System.out.println("Luas Lingkaran adalah :" +ll);
+        System.out.println("Keliling Lingkaran adalah ; " +kl);
+    }
+    
+}
---
+++ Pewaris/src/Persegi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author acer
+ */
+public class Persegi extends Bangundatar  {
+    public void Persegi(){
+        double sp,lp,kp;
+        Scanner c = new Scanner (System.in);
+        System.out.println("PERSEGI");
+        System.out.println("=========");
+        System.out.println("Masukkan sisi");
+        sp = c.nextDouble();
+        lp = sp*sp;
+        kp = 4*sp;
+        System.out.println("Luas Persegi adalah : "+lp);
+        System.out.println("Keliling Persegi adalah : "+kp);
+    }
+    
+}
