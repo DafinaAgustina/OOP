691457d9bf9355f2ca2f6d0be93fcc3c5fecf178
DafinaAgustina <56856787+DafinaAgustina@users.noreply.github.com>
2019-11-21 19:18:30 +0700

    Add files via upload

---
+++ JavaEncapsulation/src/get_set/MyClass.java
@@ -0,0 +1,19 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package get_set;
+
+/**
+ *
+ * @author acer
+ */
+public class MyClass {
+    public static void main(String[] args) {
+    MyClass myObj = new MyClass ();
+    myObj.name = "John";  // error
+    System.out.println(myObj.name); // error 
+  }
+}
---
+++ JavaEncapsulation/src/get_set/MyClass1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package get_set;
+
+/**
+ *
+ * @author acer
+ */
+public class MyClass1 { 
+  public static void main(String[] args) {
+    person myObj = new person();
+    myObj.setName("John"); // Set the value of the name variable to "John"
+    System.out.println(myObj.getName());
+  }
+}
